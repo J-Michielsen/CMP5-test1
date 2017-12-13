85bf147c61a09d80f17a37ad0f0f59539cea0237
Arnd De Cuyper <decuyperarnd@gmail.com>
2017-12-13 17:12:56 +0100

    no message

--- API delijn/index.js
+++ API delijn/index.js
@@ -12,27 +12,37 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // set the view engine to ejs
 app.set('view engine', 'ejs');
 
-var inDeBuurt;
-request('https://www.delijn.be/rise-api-core/haltes/indebuurt/103787/194286/300', function (error, response, body) {
-  console.log('Status:', response.statusCode);
-  console.log('Headers:', JSON.stringify(response.headers));
-  console.log('Response:', body);
-  inDeBuurt = body;
-});
-
-var halte;
-request('https://www.delijn.be/rise-api-core/haltes/vertrekken/201147/10', function (error, response, body) {
-  console.log('Status:', response.statusCode);
-  console.log('Headers:', JSON.stringify(response.headers));
-  console.log('Response:', body);
-  halte = body;
-});
-
 app.get('/indebuurt', function(req, res) {
-    res.render('indebuurt', {
-      data: inDeBuurt,
-      terug: ""
-    });
+
+     var inDeBuurt;
+
+     var long = req.query.long;
+     var lat = req.query.lat;
+     var coorX = 0;
+     var coorY = 0;
+     var radius = 300;
+     // omrekenen
+     request('https://www.delijn.be/rise-api-core/coordinaten/convert/' + lat + '/' + long, function (error, response, body) {
+          var response = JSON.parse(body);
+          //console.log(response.xCoordinaat);
+          coorX = response.xCoordinaat;
+          coorY = response.yCoordinaat;
+
+          console.log("x:"+coorX+"y:"+coorY);
+
+
+          request('https://www.delijn.be/rise-api-core/haltes/indebuurt/' + coorX + '/' + coorY + '/' + radius, function (error, response, body) {
+               // console.log('Status:', response.statusCode);
+               // console.log('Headers:', JSON.stringify(response.headers));
+               //console.log('Response:', body);
+               inDeBuurt = body;
+
+               //console.log(inDeBuurt);
+               res.render('indebuurt', {
+                    data: inDeBuurt
+               });
+          });
+     });
 });
 
 app.get('/', function(req, res) {
@@ -40,11 +50,21 @@ app.get('/', function(req, res) {
     });
 });
 
-app.get('/halte/:number', function(req, res) {
-    res.render('halte', {
-         data: halte,
-         terug: "indebuurt"
-    });
+app.get('/halte', function(req, res) {
+     var halte;
+     var halteNr = req.query.nr;
+     console.log(halteNr);
+     request('https://www.delijn.be/rise-api-core/haltes/vertrekken/' + halteNr + '/10', function (error, response, body) {
+       // console.log('Status:', response.statusCode);
+       // console.log('Headers:', JSON.stringify(response.headers));
+       // console.log('Response:', body);
+       halte = body;
+
+       res.render('halte', {
+           data: halte
+      });
+     });
+
 });
 
 app.use(express.static('public'))
