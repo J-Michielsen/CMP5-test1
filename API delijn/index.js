7b78e9d7d358cfad4e108b6db3779ed2fd6413c1
Arnd De Cuyper <decuyperarnd@gmail.com>
2017-12-13 11:45:05 +0100

    no message

--- API delijn/index.js
+++ API delijn/index.js
@@ -18,19 +18,30 @@ request('https://www.delijn.be/rise-api-core/haltes/indebuurt/103787/194286/300'
   console.log('Headers:', JSON.stringify(response.headers));
   console.log('Response:', body);
   inDeBuurt = body;
-      console.log(inDeBuurt);
 });
 
+var halte;
+request('https://www.delijn.be/rise-api-core/haltes/titel/200144', function (error, response, body) {
+  console.log('Status:', response.statusCode);
+  console.log('Headers:', JSON.stringify(response.headers));
+  console.log('Response:', body);
+  halte = body;
+});
 
-app.get('/', function(req, res) {
-    res.render('index', {
+app.get('/indebuurt', function(req, res) {
+    res.render('indebuurt', {
       data: inDeBuurt
     });
 });
 
 app.get('/', function(req, res) {
+    res.render('index', {
+    });
+});
+
+app.get('/halte/:number', function(req, res) {
     res.render('halte', {
-      data: d
+         data: halte
     });
 });
 
